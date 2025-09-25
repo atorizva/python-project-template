a4825ee5726f9c7593d77404c197bb0e71207522
atorizva <alfonso.toriz@outlook.com>
2025-09-24 21:17:47 -0600

    task #5: files checked under pre-commit

--- src/python_project_template/__init__.py
+++ src/python_project_template/__init__.py
@@ -1,2 +1,13 @@
+"""Sample file."""
+
+
 def hello() -> str:
+    """
+    Sample func that print that greets user.
+
+    Returns
+    -------
+    str
+        Greeting
+    """
     return "Hello from python-project-template!"
--- test/test_hello.py
+++ test/test_hello.py
@@ -1,4 +1,8 @@
+"""Sample test."""
+
 from python_project_template import hello
 
+
 def test_hello() -> None:
+    """Sample test."""
     assert hello() == "Hello from python-project-template!"
